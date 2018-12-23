8cdd558b50d83c2dd47abae98ff78ca22489392b
kitsune4126 <kitsune-san@nifty.com>
2018-12-23 10:18:29 +0900

    * メインの SpringBootApplication を、スタンドアロンの Swing アプリケーションとして動作するように修正。

--- src/main/java/com/mamezou/lentil/LentilApplication.java
+++ src/main/java/com/mamezou/lentil/LentilApplication.java
@@ -1,14 +1,27 @@
-package com.mamezou.lentil;
+package com.mamezou.lentil ;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.swing.JFrame ;
+import org.springframework.boot.autoconfigure.SpringBootApplication ;
+import org.springframework.boot.builder.SpringApplicationBuilder ;
+import org.springframework.context.ConfigurableApplicationContext ;
 
 @SpringBootApplication
 public class LentilApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LentilApplication.class, args);
-	}
+    public static void main( final String ... args ) {
+        try ( final ConfigurableApplicationContext context = new SpringApplicationBuilder( LentilApplication.class ).headless( false ).run( args ) ) {
+            LentilApplication application = context.getBean( LentilApplication.class ) ;
+            application.run( args ) ;
+        } catch ( Exception e ) {
+            e.printStackTrace() ;
+        }
+    }
+
+    public void run( String ... args ) throws Exception {
+        final JFrame frame = new JFrame( "Lentil Main Window" ) ;
+        frame.setSize( 640 , 480 ) ;
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE ) ;
+        frame.setVisible( true ) ;
+    }
 
 }
-
