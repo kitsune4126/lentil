b1fbfb8c7aa4235bbe3d46e603ab8fad452d4b8a
kitsune4126 <kitsune-san@nifty.com>
2018-12-23 10:14:57 +0900

    * SPRING INITIALIZR ( https://start.spring.io/ ) で生成したプレーンなファイル群を追加。

---
+++ src/main/java/com/mamezou/lentil/LentilApplication.java
@@ -0,0 +1,14 @@
+package com.mamezou.lentil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LentilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LentilApplication.class, args);
+	}
+
+}
+
